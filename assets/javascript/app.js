76124e00fd224535ede430db304e9e1255df0560
Max Yao <bojinyao@berkeley.edu>
2018-06-28 19:09:37 -0700

    setting up datebase stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,31 @@ var config = {
     messagingSenderId: "693263172630"
   };
   firebase.initializeApp(config);
+
+
+  var database = firebase.database();
+  $(document).ready(function() {
+
+    $(".submit").on("click", function(event) {
+        event.preventDefault();
+  
+        name = $(".employee-name").val().trim();
+        role = $(".role").val().trim();
+        date = $("start-date").val().trim();
+        rate = $(".monthly-rate").val().trim();
+  
+        database.ref().set({
+          name: name,
+          role: role,
+          date: date,
+          rate: rate
+        });
+  
+      });
+    
+
+
+
+
+
+  });
