4f0ca848df5a936e63d2576cdbb2d318fb7b3275
Max Yao <bojinyao@berkeley.edu>
2018-06-28 19:01:14 -0700

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCNj9Lc04gsBw-CeP4C0HZ9-k-qye4y2Rk",
+    authDomain: "employee-data-management-bebe7.firebaseapp.com",
+    databaseURL: "https://employee-data-management-bebe7.firebaseio.com",
+    projectId: "employee-data-management-bebe7",
+    storageBucket: "",
+    messagingSenderId: "693263172630"
+  };
+  firebase.initializeApp(config);
